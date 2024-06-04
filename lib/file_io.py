71e584c71eb62fdf12e0b52845d1f91e60184df9
victor kamau <vikakamau04@gmail.com>
2024-06-04 13:05:48 +0300

    initial

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(str(file_name) +'.txt', 'w') as file:
+     file.write(str(file_content))
 
 def append_file(file_name, append_content):
-    pass
+    with open(str(file_name) +'.txt', 'a') as file:
+       file.write(str(append_content))
 
 def read_file(file_name):
-    pass
+   with open(str(file_name) +'.txt', 'r') as file:
+      file_content_read = file.read()
+      return file_content_read
